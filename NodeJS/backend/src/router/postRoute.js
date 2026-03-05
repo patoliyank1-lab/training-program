87eafc8398534e18958375d1de00de70e89ec2db
NikheelK <patoliya.nk1@gmail.com>
2026-03-05 18:05:52 +0530

    feat: add route for createpost

--- NodeJS/backend/src/router/postRoute.js
+++ NodeJS/backend/src/router/postRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getAllPost, getPostById, updatePostById, deletePostById } from '../controllers/postCtr.js';
+import { getAllPost, getPostById, updatePostById, deletePostById,createNewPost } from '../controllers/postCtr.js';
 
 const router = express.Router()
 
 //post routes
 router.get('/', getAllPost);
 router.get('/:id', getPostById);
+router.post('/:id', createNewPost);
 router.put('/:id', updatePostById);
 router.delete('/:id', deletePostById);
 
